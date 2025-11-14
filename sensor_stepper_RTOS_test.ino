5819bc311719321f03acb5a2a1129df90059cffb
jtbee <93067219+Xaneak@users.noreply.github.com>
2025-11-14 18:03:40 -0500

    Updated program code

--- sensor_stepper_RTOS_test.ino
+++ sensor_stepper_RTOS_test.ino
@@ -36,11 +36,17 @@ const int stepSequence[8][4] = {
 typedef enum { STATE_OFF, STATE_IDLE, STATE_SEARCH, STATE_TARGET,
                STATE_TRACKING, STATE_ERROR } turret_state_t;
 typedef enum { EV_POWER_ON, EV_FOUND, EV_LOST, EV_LEFT,
-               EV_RIGHT, EV_ERROR} system_event_t;
+               EV_RIGHT, EV_TIMEOUT, EV_ERROR} system_event_t;
 
 // ----- RTOS objects -----
 QueueHandle_t xEventQueue;
 SemaphoreHandle_t xStateMutex;
+
+// ----- Timer callbacks -----
+void searchTimerCallback(TimerHandle_t){ system_event_t e=EV_TIMEOUT; xQueueSend(xEventQueue,&e,0); }
+
+// ----- Sensor Stuff -----
+int sensor_mode = 0;
 
 inline void pushEvent(system_event_t ev){ 
     xQueueSend(xEventQueue, &ev, 0); 
@@ -61,24 +67,28 @@ void stateMachineTask(void *){
         // Move Position of Assembly Stepper Motor to Match Sensor Stepper Motor
         // Move to Target State
       }
-      else if ((currentState==STATE_TARGET && e==EV_LEFT) || (currentState==STATE_TARGET && e==EV_RIGHT)) {
-        // Move to Tracking State
+      else if (currentState==STATE_TARGET) {
+        // May need a "firing" state
+        // "Fire" Laser i.e. Blink Laser
       }
-      else if (currentState==STATE_TRACKING && e==EV_LEFT) {
+      /*else if ((currentState==STATE_TARGET && e==EV_LEFT) || (currentState==STATE_TARGET && e==EV_RIGHT)) {
+        // Move to Tracking State
+        // Set sensor_mode flag
+      }*/
+      // NEED TO ADD - Flag to stop both of these to fire simulataneously.
+      // i.e. Which ever fires first will maintain its movement
+      else if (currentState==STATE_TARGET && e==EV_LEFT) {
         // Start Assembly Stepper Motor moving Left
         // Start Out-of-Range Timer
       }
-      else if (currentState==STATE_TRACKING && e==EV_RIGHT) {
+      else if (currentState==STATE_TARGET && e==EV_RIGHT) {
         // Start Assembly Stepper Motor moving right
         // Start Out-of-Range Timer
       }
-      else if (currentState==STATE_TRACKING && e==EV_FOUND) {
+      /*else if (currentState==STATE_TRACKING && e==EV_FOUND) {
         // Stop Assembly Stepper Motor
-        // May need a "firing" state
-        // "Fire" Laser
-        // Move Back to Target State
-      }
-      else if (currentState==STATE_TRACKING && e==EV_LOST) {
+      }*/
+      else if (currentState==STATE_TRACKING && e==EV_TIMEOUT) {
         // Stop Assembly Stepper Motor
         // Move Back to Search State
       }
@@ -108,12 +118,35 @@ void stateSensorTask(void *) {
         if (currentState==STATE_SEARCH) {
           pushEvent(EV_FOUND);
         }
-        else {
+        else if (currentState==STATE_TRACKING) {
           pushEvent(EV_RIGHT);
         }
+        else {
+          // Do Nothing
+        }
       }
-      else if (right_distance >= 10 && right_distance <= 20){
+      if (right_distance >= 10 && right_distance <= 20){
+        if (currentState==STATE_SEARCH) {
+          pushEvent(EV_FOUND);
+        }
+        else if (currentState==STATE_TRACKING) {
+          pushEvent(EV_LEFT);
+        }
+        else {
+          // Do Nothing
+        }
+      }
+      // Both Lost
+      if ((left_distance < 20) && (right_distance < 20)) {
+        pushEvent(EV_LOST);
+      }
+      // Right Lost
+      if ((right_distance < 20) && (right_distance >= 10 && right_distance <= 20)) {
         pushEvent(EV_LEFT);
+      }
+      // Left Lost
+      if ((left_distance < 20) && (left_distance >= 10 && left_distance <= 20)) {
+        pushEvent(EV_RIGHT);
       }
       /*if(distance >= 10 && distance <= 20){
         // Move forward 180°
@@ -137,20 +170,35 @@ void stateSensorTask(void *) {
 
 void setup() {
   // put your setup code here, to run once:
-  xTaskCreate(TaskPanning, // Task function
-              "Panning", // Task name
-              128, // Stack size 
-              NULL, 
-              0, // Priority
-              &taskTesting); // Task handler
 
-  pinMode(TRIG, OUTPUT);
-  pinMode(ECHO, INPUT);
-  
   Serial.begin(9600);
+
+  pinMode(IN1_0, OUTPUT);
+  pinMode(IN2_0, OUTPUT);
+  pinMode(IN3_0, OUTPUT);
+  pinMode(IN4_0, OUTPUT);
+
+  pinMode(IN1_1, OUTPUT);
+  pinMode(IN2_1, OUTPUT);
+  pinMode(IN3_1, OUTPUT);
+  pinMode(IN4_1, OUTPUT);
+
+  pinMode(TRIG_0, OUTPUT);
+  pinMode(ECHO_0, INPUT);
+  pinMode(TRIG_1, OUTPUT);
+  pinMode(ECHO_1, INPUT);
+
+  xEventQueue=xQueueCreate(10,sizeof(system_event_t));
+  xStateMutex=xSemaphoreCreateMutex();
+
+  xSearchTimer = xTimerCreate("Search Timeout", pdMS_TO_TICKS(10000), pdFALSE, NULL, searchTimerCallback);
+
+  xTaskCreatePinnedToCore(stateMachineTask,"FSM",4096,NULL,2,NULL,1);
+  xTaskCreatePinnedToCore(stateSensorTask, "Sensors Task", 4096, NULL, 1, NULL, 1);
+  Serial.println("=== Turret Project: Prototype ===");
+  
 }
 
-void TaskPanning(void *pvParameters) {
   (void) pvParameters;
   for (;;)
   {
